5c80bcdc6d16b34a582164cd921a5c0417f30e27
maziar <sojodyan@gmail.com>
2024-10-02 16:12:42 -0400

    update config

--- nvim/init.lua
+++ nvim/init.lua
@@ -88,9 +88,27 @@ cmp.setup({
     end,
   },
   mapping = {
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif require('luasnip').expand_or_jumpable() then
+        require('luasnip').expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif require('luasnip').jumpable(-1) then
+        require('luasnip').jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item
     ['<C-e>'] = cmp.mapping.close(),
   },
   sources = {
